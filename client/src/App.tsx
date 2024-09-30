352041908a38d29b9454adca70b7e116f792e297
AyRawat <aayushrawat20@gmail.com>
2024-09-30 18:23:35 +0530

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { Navbar, Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar className="border-b-2">
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            BlogPro
+          </span>
+          Blog
+        </Link>
+        <div className="">
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        </div>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
