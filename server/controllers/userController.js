da54b1d1cf9679515a4e04fa37fa5329523ac364
AyRawat <aayushrawat20@gmail.com>
2024-09-30 19:17:07 +0530

    sign-up api

---
+++ server/controllers/userController.js
@@ -0,0 +1,75 @@
+const User = require("../models/User");
+
+const { createSecretToken } = require("../auth/generateToken");
+
+const bcrypt = require("bcrypt");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!(email && password)) {
+    return res.status(400).json({ message: "All input is required" });
+  }
+  const user = await User.findOne({ email });
+  if (!(user && (await bcrypt.compare(password, user.password)))) {
+    return res.status(404).json({ message: "Invalid credentials" });
+  }
+  const token = createSecretToken(user._id);
+  res.cookie("token", token, {
+    domain: process.env.frontend_url, // Set your domain here
+    path: "/", // Cookie is accessible from all paths
+    expires: new Date(Date.now() + 86400000), // Cookie expires in 1 day
+    secure: true, // Cookie will only be sent over HTTPS
+    httpOnly: true, // Cookie cannot be accessed via client-side scripts
+    sameSite: "None",
+  });
+
+  res.json({ token });
+};
+
+const createUser = async (req, res) => {
+  try {
+    if (
+      !(
+        req.body.email &&
+        req.body.password &&
+        req.body.name &&
+        req.body.username
+      )
+    ) {
+      res.status(400).send("All input is required");
+    }
+
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      name: req.body.name,
+
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    const token = createSecretToken(user._id);
+
+    res.cookie("token", token, {
+      path: "/", // Cookie is accessible from all paths
+      expires: new Date(Date.now() + 86400000), // Cookie expires in 1 day
+      secure: true, // Cookie will only be sent over HTTPS
+      httpOnly: true, // Cookie cannot be accessed via client-side scripts
+      sameSite: "None",
+    });
+
+    console.log("cookie set succesfully");
+
+    res.json(user);
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
+};
+
+module.exports = { login, createUser };
